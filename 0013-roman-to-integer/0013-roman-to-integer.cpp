cf6102b212597f9924a09ee4976cfe51bb7b27d6
Lavanya Garg <148633090+flutebyte@users.noreply.github.com>
2026-02-12 12:03:38 +0530

    Time: 3 ms (74.04%), Space: 12.3 MB (62.05%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res=0;
+        unordered_map<char, int> dict;
+        dict['I']=1;
+        dict['V']=5;
+        dict['X']=10;
+        dict['L']=50;
+        dict['C']=100;
+        dict['D']=500;
+        dict['M']=1000;
+        for(int i=0;i<s.size();i++){
+            auto it = dict.find(s[i]);
+            int curr = dict[s[i]];
+            int next= dict[s[i+1]];
+            if(curr>=next){
+                res+=curr;
+            }else{
+                int x= next-curr;
+                res+=x;
+                i++;
+            }
+        }
+        return res;
+    }
+};
