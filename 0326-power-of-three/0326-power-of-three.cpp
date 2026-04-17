d952763ca350a7225e839efa5369e628c181f41d
Lavanya Garg <148633090+flutebyte@users.noreply.github.com>
2026-04-17 22:21:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (9.64%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        while(n%3==0 && n>=3){
+            n=n/3;
+        }
+        if(n==1) return true;
+        else return false;
+    }
+};
