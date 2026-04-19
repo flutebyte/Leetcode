1044a848e8be7f7b30378d5a87f1e22f68116227
Lavanya Garg <148633090+flutebyte@users.noreply.github.com>
2026-04-19 11:53:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (16.43%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         unordered_map<char, int > mpp;
-        string alp = "abcdefghijklmnopqrstuvwxyz";
-        for(char c:alp){
-            mpp[c]=0;
-        }
+        // string alp = "abcdefghijklmnopqrstuvwxyz";
+        // for(char c:alp){
+        //     mpp[c]=0;
+        // }
+        // for(char c:sentence){
+        //     mpp[c]++;
+        // }
+        // for(char c:alp){
+        //     if(mpp[c]==0) return false;
+        // }
+        // return true;
+        int count =0;
         for(char c:sentence){
-            mpp[c]++;
+            auto it = mpp.find(c);
+            if(it==mpp.end()){
+                mpp[c]++;
+                count++;
+            }
+            else continue;
         }
-        for(char c:alp){
-            if(mpp[c]==0) return false;
-        }
-        return true;
+        if(count==26) return true;
+        return false;
     }
 };
